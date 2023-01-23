3f4e5842e03f0852c1c44924a59252d7ca35fc15
NRUBYNATHAN <rubynathan999@gmail.com>
2023-01-23 11:07:02 +0530

    import

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 
-
 import "./App.css"
 //named import
 import {useState} from "react";
-
-
 //default export
 export default function App(){
        return(
@@ -115,7 +112,6 @@ function Movielist(){
  
         );
 }
-
 function Movie({data}){
 //conditional styling
   const styling={
@@ -150,9 +146,6 @@ return(
 </div>
 );
 }
-
-
-
 function Counter() {
 
 
