8ba7f1d306162a67acd6767b20283637d774985c
NRUBYNATHAN <rubynathan999@gmail.com>
2023-01-24 18:48:22 +0530

    export

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,18 @@ import {useState} from "react";
 //
 import IconButton from '@mui/material/IconButton';
 //default export
-//
 import DeleteIcon from '@mui/icons-material/Delete';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+//example import counter
+import { Counter } from "./Counter";
+
+
 function App(){
        return(
                <div>
-                  <Movielist/> 
+                  {/* <Counter/>  */}
+                  <Movielist/>
                   
                 </div>
               );
@@ -178,29 +182,5 @@ return(
 </div>
 );
 }
-function Counter() {
-
-
-  const [total, setLike] = useState(0);
-
-  const [total1, setLike1] = useState(0);
-
-  return (
-
-    <div className="like">
-      <IconButton onClick={() => setLike(total + 1)} aria-label="delete" color="primary">
-      👍{total}
-     </IconButton>
-     <IconButton onClick={() => setLike1(total1+ 1)} aria-label="delete" color="error">
-     👎{total1}
-     </IconButton>
-
-      {/* <button className="likes" onClick={() => setLike(total + 1)}>👍{total}</button>
-      <button className="likes" onClick={() => setLike1(total1 + 1)}>👎{total1}</button> */}
-
-    </div>
-  );
-}
-
 //export app
 export default App;
